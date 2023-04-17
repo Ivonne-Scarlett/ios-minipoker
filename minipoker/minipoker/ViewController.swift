79e90a98e25d5714731ed7e695a0fd01cab09fbd
Ivonne <ivonne.scarlett01@gmail.com>
2023-04-17 10:38:46 -0600

    Tranferir proyecto a GitHub

---
+++ minipoker/minipoker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  minipoker
+//
+//  Created by Juan de Dios Vargas on 13/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
